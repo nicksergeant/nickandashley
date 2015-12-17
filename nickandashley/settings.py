808e7881b60d1c14a15a66f27351e4abacc99c17
Nick Sergeant <nick@nicksergeant.com>
2015-12-17 14:47:15 -0500

    Change IP.

--- nickandashley/settings.py
+++ nickandashley/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'nickandashley',
         'USER': 'nickandashley',
         'PASSWORD': os.environ['PSQL_PASSWORD'],
-        'HOST': '0.0.0.0',
+        'HOST': '45.79.167.1',
         'PORT': '5432',
     }
 }
